2ba94efc6973e537fe2f4c87ba5b1e3c69c71a8d
Tanishq Pauskar <50085723+PuddlesPenguin@users.noreply.github.com>
2026-05-31 23:29:42 -0400

    committing new changes

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,6 @@
-const {Client} = require('pg');
+const { Pool } = require('pg');
 
-const client = new Client({
-    host: 'localhost',
-    port: 5432,
-    user: 'tpauskar',
-    password: 'Puddles_Penguin1',
-    database: 'potw'
-});
+require('dotenv').config();
+
+const pool = new Pool({
+  user: 'tpauskar',
